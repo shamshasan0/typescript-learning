784e665eab399a4dd91ba7a589b71ab33e45371d
Shams Hasan <shamshasan941@gmail.com>
2024-04-07 18:31:42 -0400

    practiced arithmetic functions

---
+++ basics.ts
@@ -0,0 +1,31 @@
+// divide two numbers
+function add(a: number, b: number) {
+    return a + b;
+}
+
+const sum = add(2, 2);
+
+console.log(sum) // 4
+
+
+// divide two numbers
+function multiply(a: number, b: number) {
+    return a * b;
+}
+
+const product = multiply(3, 3);
+
+console.log(product); // 9
+
+// divide two numbers
+function divide(a: number, b: number) {
+    return b / a;
+}
+const quotient = divide(3, 127);
+
+console.log(quotient.toFixed(1)); // round to one decimal place; 42.3
+
+
+
+
+

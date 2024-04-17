ecb9b0b595dd958ca0e22db27013af11b0311f71
Shams Hasan <105460072+shamshasan0@users.noreply.github.com>
2024-04-17 16:09:42 -0400

    Update findMaximum.ts
    added in test cases; considered the edge case of negative numbers in an array

--- findMaximum.ts
+++ findMaximum.ts
@@ -1,13 +1,19 @@
 // find the maximum element in an array
 function findMaximum(numbers: number[]) {
-    let maximum: number = numbers[0];
-    for (let value of numbers) {
-        if (value > maximum) {
-            maximum = value;
+
+    let maximum: number = -Infinity // set maximum equal to the lowest possible negative value
+    for (let value of numbers) { // go through all elements in the numbers array
+        if (value > maximum) { // check if that element is bigger than the current maximum
+            maximum = value; // if so, set maximum equal to that element
         }
     }
     return maximum;
 }
 
-const maxResult = findMaximum([1, 2, 3, 4, 5])
-console.log(maxResult) // 5
+//tests
+const test1 = findMaximum([-20, -52, -5, -32, -90])
+const test2 = findMaximum([10, 9, 4, 19, 12])
+
+console.log(test1); // -5
+console.log(test2); // 19
+

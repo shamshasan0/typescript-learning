5cadf49995ed7d9341e2a888383cfddf215f3f1f
Shams Hasan <105460072+shamshasan0@users.noreply.github.com>
2024-05-14 01:00:19 -0400

    Update generics.ts

--- generics.ts
+++ generics.ts
@@ -1,27 +1,28 @@
-// Generics
+//  GENERICS
 
 /* Generics allow for flexibility in TS functions. 
-TS will read parameter type(s), then helps to output and return appropiate type(s) based on that. */
+TS reads parameter type(s), then helps to output and return appropiate type(s) based on that. */
 
 function insertAtBeginning<T>(array: T[], value: T) {  // both the array and the value passed in are now of Type T
-    const newArray = [value, ...array]; // spread operator copies current array contents into the new array, and then the value gets inserted at the beginning
+    const newArray = [value, ...array]; // ... spread operator copies current array contents into the new array; the value gets inserted at the beginning of the array
 
-    return newArray;
-}
+    function insertAtBeginning<T>(array: T[], value: T) {  // both the array and the value passed in are now of Type T
+        const newArray = [value, ...array]; // spread operator copies current array contents into the new array, and then the value gets inserted at the beginning
 
-/* function is called; parameters of string array and string are determined. 'a' has been inserted at the start of the array. */
-const alphabetArray = insertAtBeginning(['b', 'c', 'd', 'e'], 'a');
+        return newArray;
+    }
 
-
-console.log(alphabetArray); // Output: [ 'a', 'b', 'c', 'd', 'e' ] 
-
-
+    /* function is called; parameters of string array and string are determined. 'a' has been inserted at the start of the array. */
+    const alphabetArray = insertAtBeginning(['b', 'c', 'd', 'e'], 'a');
 
 
-function joinArrays<T>(arr1: T[], arr2: T[]) { // two arrays of Generic type T
-    const newArray = arr1.concat(arr2); // joins the two arrays
-    return newArray;
-}
+    console.log(alphabetArray); // Output: [ 'a', 'b', 'c', 'd', 'e' ] 
 
-/* function is called and the arrays are determined to be of type number, therefore this function returns us a numbers array */
-console.log(joinArrays([1, 2, 3, 4], [5, 6, 7, 8])) //  Output:  [ 1,2,3,4,5,6,7,8 ]
+
+
+    function joinArrays<T>(arr1: T[], arr2: T[]) { // two arrays of Generic type T
+        const newArray = arr1.concat(arr2); // joins the two arrays
+        return newArray;
+    }
+
+    console.log(joinArrays([1, 2, 3, 4], [5, 6, 7, 8])) //  Output:  [ 1,2,3,4,5,6,7,8 ]
